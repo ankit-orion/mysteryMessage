6c10005d164623f0d654ca82a7d87f543c73a298
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-07-28 16:39:38 +0530

    chore: Add react-email dependency and implement email verification functionality

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,1 +1,23 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email:string,
+    username: string,
+    verifyCode:string
+):Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.com',
+            to: email,
+            subject: 'Mystery Message | Verification Code',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+        return {success: true, message: "Verification email sent"};
+        
+    } catch (emailEroor) {
+        console.log("Error sending verification email", emailEroor);
+        return{success: false, message:"Error sending verification email"};
+    }
+}
---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,7 @@
+import { Message } from "@/model/User";
+export interface ApiResponse{
+    success: boolean;
+    message: string;
+    isAcceptingMessages?: boolean; // this is optional
+    messages?: Array<Message>
+}
